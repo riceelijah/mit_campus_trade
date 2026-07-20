2030332ddfa0a09792871485e3fbde9270ff72e1
Elijah Rice <ejrice@mit.edu>
2026-07-19 20:14:28 -0400

    initial commit

---
+++ src/card.ts
@@ -0,0 +1,6 @@
+export class Card {
+    /**
+     * AF(number, id): A controlled, digital representation of the MIT Campus Trade trading card with the same
+     *  collection mumber, corresponding name, and the same id number which distinguishes it from other cards of its number
+     */
+}
